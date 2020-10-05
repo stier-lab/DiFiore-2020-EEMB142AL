4bff419fd237a8283123c5c4ba8db86d80a963d5
Bart DiFiore <BartMBP@MacBook-Pro-6.local>
2020-10-05 10:37:11 -0400

    Updated initLesson.R

--- Working_with_data/initLesson.R
+++ Working_with_data/initLesson.R
@@ -10,7 +10,7 @@ swirl_options(swirl_logging = TRUE)
 
 # Path to data
 .datapath <- file.path(.get_course_path(),
-                      'DiFiore-2020-EEMB142AL', 'Vectors',
+                      'DiFiore-2020-EEMB142AL', 'Working_with_data',
                       'Biocom_pred_prey_diet.csv')
 
 #-----------------------------------------------------
@@ -27,6 +27,9 @@ fish <- read.csv(.datapath, na.strings ="na") %>%
   mutate(total = sum(c_across(net_wt_odonata:net_wt_terrestrial_inverts), na.rm = T), 
          net_wt_fish = sum(c_across(c(net_wt_fish,net_wt_smelt, net_wt_bluntnose, net_wt_mimicshiner, net_wt_other_fish)), na.rm = T), 
          net_wt_other = net_wt_odonata + net_wt_ephemeroptera + net_wt_diptera + net_wt_trichoptera  + net_wt_hemiptera + net_wt_cladocera + net_wt_hydrocarina + net_wt_coleopterans + net_wt_amphipod + net_wt_emergent_aquatics + net_wt_terrestrial_inverts) %>%
-  select(year, sampledate, species, gutlabel, length, weight, total, net_wt_fish, net_wt_crayfish, net_wt_other)
+  select(year, sampledate, species, gutlabel, length, weight, total, net_wt_fish, net_wt_crayfish, net_wt_other) %>%
+  ungroup()
+
+fish <- as.data.frame(fish)
 
 
